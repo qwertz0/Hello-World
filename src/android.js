b84f23a66ed75fe5ac99a19d7d42f34535a77815
qwertz0 <oxha6p20t68uh9x66ybb@vermutlich.net>
2018-04-21 19:17:49 +0200

    Update android.js

--- src/android.js
+++ src/android.js
@@ -1,7 +1,7 @@
 function start() {
 	importFiles({
-		js:["dev/src/base.js"],
-		css:["dev/src/base.css"]
+		js:["https://raw.githubusercontent.com/qwertz0/Hello-World/master/src/base.js"],
+		css:["https://raw.githubusercontent.com/qwertz0/Hello-World/master/src/base.css"]
 	},()=>los());
 }
 
